6345126151dece8a78733fb825c2e1c2509b503f
admin <torrentdu64@gmail.com>
2020-07-17 14:45:32 +1200

    Initiate the project

---
+++ Assignment2Torrent09196576/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Assignment2Torrent09196576.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Assignment2Torrent09196576.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Assignment2Torrent09196576.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
